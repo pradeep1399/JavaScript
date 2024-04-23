dd4a485d1e558a185bf0fd5f1ba5dcddfd588e5f
Pradeep Kumar Verma <85738548+pradeep1399@users.noreply.github.com>
2024-04-23 23:47:45 +0530

    Add files via upload

---
+++ CallBacks&Promises/app.js
@@ -0,0 +1,9 @@
+// async await >> promise chains >> callback hell
+
+/*
+Synchronous: Synchronous means the code runs in a particular sequence of instructions given in the program. 
+Each instruction waits for the previous instruction to complate its execution.
+
+Asynchronous: Due to synchronous programming, sometimes imp instruction get blocked due to some previous instruction, which causes a delay in the UI.
+Asynchronous code execution allows to execute next instructions immediately and doesn't block the flow.
+*/
