3312623d0ba85801ca6e360bc423b8ea3390d3b4
pradeep1399 <pradeepverma1399@gmail.com>
2024-04-25 01:06:20 +0530

    Async-Await

--- CallBacks&Promises/app.js
+++ CallBacks&Promises/app.js
@@ -22,8 +22,30 @@ A JavaScript Promise object can be:
 
 ** Promise has state(pending, fulfilled) & some result (result for resolve & error for reject).
 ** resolve & reject are callbacks provided by JS
-*/
 
+//Async-Await
+async function always return a promise.
+async function myFunc(){...}
+await pause the execution of its surrounding async function until the promise is settled.
+
+IIFE: Immediately Invoked Function Expression
+IIFE is a function that is called immediately as soon as it is defined.
+    three way to define
+        1).
+        (function (){
+            ....
+        })();
+
+        2).
+        (() => {
+            ....
+        })();
+
+        3).
+        (async () => {
+            ....
+        })();
+*/
 
 //######################################################################################
 /*
@@ -66,8 +88,6 @@ getData(234);
 getData(345);
 */
 
-
-
 //##############################################################################
 /*
 function getData(dataId, getNextData) {
@@ -99,12 +119,130 @@ getData(1, () => {
 //     reject("some error occurred");
 // });
 
+// function getData(dataId, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataId);
+//       resolve("success");
+//     }, 5000);
+//   });
+// }
 
-function getData(dataId, getNextData) {
+// .then() 7 .catch()
+// promiseHooks.then((res) => {...});
+// promiseHooks.catch((err) => {...});
+
+//##############################################################################
+/*
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("I am a promise");
+        resolve("success");
+        // reject("network error");
+    });
+};
+
+let promise=  getPromise();
+promise.then((res) => {
+    console.log("promise fulfilled", res);
+});
+
+promise.catch((err) => {
+    console.log("reject", err);
+});
+*/ /*
+
+function asyncFunc1() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("data", dataId);
+            console.log("data1");
             resolve("success");
-        }, 5000);
+        }, 2000);
     });
+};
+
+function asyncFunc2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data2");
+            resolve("success");
+        }, 2000);
+    });
+};
+
+console.log("fetching data1....");
+asyncFunc1().then((res) => {
+    console.log("fetching data2...");
+    asyncFunc2().then((res) => {
+    });
+});
+*/
+
+//##############################################################################
+/*
+function getData(dataId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve("success");
+    }, 2000);
+  });
 }
+
+//Promise chain
+getData(1)
+  .then((res) => {
+    return getData(2);
+  })
+  .then((res) => {
+    return getData(3);
+  })
+  .then((res) => {
+    console.log(res);
+  });
+*/
+
+//##############################################################################
+//Async-Await
+
+function api(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("weather data", data);
+      resolve(200);
+    }, 2000);
+  });
+}
+
+async function getWeatherData() {
+  await api("good");
+  await api("moderate");
+}
+
+function getData(dataId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve("success");
+    }, 2000);
+  });
+}
+
+// async function getAllData() {
+//   console.log("getting data1...");
+//   await getData(1);
+//   console.log("getting data2...");
+//   await getData(2);
+//   console.log("getting data3...");
+//   await getData(3);
+// }
+
+// IIFE
+(async function () {
+  console.log("getting data1...");
+  await getData(1);
+  console.log("getting data2...");
+  await getData(2);
+  console.log("getting data3...");
+  await getData(3);
+})();
